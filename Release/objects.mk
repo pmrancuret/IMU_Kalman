1b72bb1dcd727b865a029c89dd0254fc7676440f
Paul Rancuret <pmrancuret@gmail.com>
2014-01-18 18:35:59 -0500

    Removed Release/ directory contents from index.

--- Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lArduino_Duemilanove_w__ATmega328 -lavrfix -lWire -lSPI -lMPU6000 -lHMC5883 -lGyroKalman
-
